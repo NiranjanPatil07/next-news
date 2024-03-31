e62284a31435bea0024f70d00c97a61156c460a5
Niranjan Patil <103982890+NiranjanPatil07@users.noreply.github.com>
2024-03-31 22:57:38 +0530

    UX: Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1>
+      <Button variant={"outline"}>Button</Button>
+    </h1>
+  );
+}
